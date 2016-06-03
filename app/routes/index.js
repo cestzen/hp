d105dab9b12300fae61980c3f58136c77a10a963
C'est Zen <sezen@rocketmail.com>
2016-06-03 08:18:22 +0000

    done

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,14 +3,12 @@
 var path = process.cwd();
 
 module.exports = function (app) {
-	app.route('/')
+	app.enable('trust proxy');
+		app.route('/')
 		.get(function (req, res) {
-			var sent="";
-			
-			res.send(sent);
+			var ip = req.ip;
+			var lang = req.acceptsLanguages()[0];
+			var userAgent = req.headers['user-agent'].split('(')[1].split(')')[0];
+			res.send("{\"ipaddress\": \""+ ip + "\", \"language\": \"" + lang + "\", \"software\": \"" + userAgent+"\"}");
 		});
-
-
-
-
 };
